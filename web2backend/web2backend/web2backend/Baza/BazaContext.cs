6fa93c140c83607884f6d7a9f9c0875c5eaa3333
Luka <jeftic.luka1@gmail.com>
2020-08-23 11:50:39 +0200

    connected to database

---
+++ web2backend/web2backend/web2backend/Baza/BazaContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using web2backend.Models;
+
+namespace web2backend.Baza
+{
+    public class BazaContext : DbContext
+    {
+        public BazaContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
