24ef16d22a6ef3ea0f3ebfa8ff90b05b8e3a3a29
Luka <jeftic.luka1@gmail.com>
2020-09-04 20:57:56 +0200

    created asp net core 3.1

---
+++ web2backend/web2_server/web2_server/Controllers/HomeController.cs
@@ -0,0 +1,10 @@
+﻿namespace web2_server.Controllers
+{
+    using System.Diagnostics;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.Logging;
+
+    public class HomeController : Controller
+    {
+    }
+}
